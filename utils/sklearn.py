6d568e16d1504294bcde981e9c6cbc6e54a6d3b0
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-06-30 16:41:27 +0200

    feature(SKLearn): added sklearn pipeline

---
+++ utils/sklearn.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import FunctionTransformer
+import swifter
+
+def pipelinize(function, active=True):
+    def list_comprehend_a_function(list_or_series):
+        return list_or_series.swifter.apply(function)
+
+    return FunctionTransformer(list_comprehend_a_function, validate=False)
