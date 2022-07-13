d3a487f9156e7655d64750f0498dab29e9fba3cd
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-07-13 11:14:30 +0200

    feat: Migrated from branch #pytorch

--- library/__init__.py
+++ library/__init__.py
@@ -1,2 +1,3 @@
 from .examples.simple_pipeline import simple_pipeline
 from .examples.text_image_pipeline import text_image_pipeline
+from .examples.simple_decoder import simple_decoder
--- run_pipeline.py
+++ run_pipeline.py
@@ -3,10 +3,10 @@ from configs.constants import Const
 from data.dataloader import load_data
 from configs.config import global_preprocess_config
 from runner.run import train_pipeline
-from library import simple_pipeline
+from library import simple_pipeline, simple_decoder
 
 train_dataset, test_dataset = load_data("data/original", global_preprocess_config)
 
 train_pipeline(
-    simple_pipeline(), {"input": train_dataset}, train_dataset[Const.label_col]
+    simple_decoder(), {"input": train_dataset}, train_dataset[Const.label_col]
 )
