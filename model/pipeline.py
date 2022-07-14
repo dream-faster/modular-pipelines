2dce4c0a3da8b00625b5bcd67444c76b9ecce394
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-07-14 14:20:59 +0200

    feature(Pipeline): can get Pipeline as input (no need to use Concat as an intermediary) (#37)

--- model/pipeline.py
+++ model/pipeline.py
@@ -4,15 +4,17 @@ from typing import List, Union
 from runner.train import train_predict, predict
 from runner.store import Store
 
-
 class Pipeline(Block):
 
     id: str
-    datasource: DataSource
+    datasource: Union[DataSource, "Pipeline"]
     models: List[Model]
 
     def __init__(
-        self, id: str, datasource: DataSource, models: Union[List[Model], Model]
+        self,
+        id: str,
+        datasource: Union[DataSource, "Pipeline"],
+        models: Union[List[Model], Model],
     ):
         self.id = id
         if isinstance(models, list):
@@ -27,13 +29,13 @@ class Pipeline(Block):
             model.preload()
 
     def fit(self, store: Store) -> None:
-        last_output = self.datasource.deplate(store)
+        last_output = process_block(self.datasource, store)
         for model in self.models:
             last_output = train_predict(model, last_output, store)
         store.set_data(self.id, last_output)
 
     def predict(self, store: Store) -> pd.DataFrame:
-        last_output = self.datasource.deplate(store)
+        last_output = process_block(self.datasource, store)
         for model in self.models:
             last_output = predict(model, last_output)
         store.set_data(self.id, last_output)
@@ -41,3 +43,14 @@ class Pipeline(Block):
 
     def is_fitted(self) -> bool:
         return all([model.is_fitted() for model in self.models])
+
+
+def process_block(block: Union[DataSource, Pipeline], store: Store) -> pd.DataFrame:
+    if isinstance(block, DataSource):
+        return block.deplate(store)
+    elif isinstance(block, Pipeline):
+        if not block.is_fitted():
+            block.fit(store)
+        return block.predict(store)
+    else:
+        raise ValueError("Expected DataBlock or Pipeline")
