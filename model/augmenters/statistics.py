db380554c66aacedf619e062231356fe6b3fdd12
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-07-14 09:01:31 +0200

    fix(Augmenter): Statistic Augmenter improved. (#34)
    * feat: Specified better aggressive char.
    
    * feat: Outliers are only counted when text is large enough.
    
    Co-authored-by: Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>

--- model/augmenters/statistics.py
+++ model/augmenters/statistics.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk.corpus import wordnet as wn
-from typing import List, Union, Optional
+from typing import List, Any, Union, Tuple, Optional
 import pandas as pd
 import spacy
 from type import BaseConfig
@@ -10,6 +10,7 @@ from urlextract import URLExtract
 from .base import Augmenter
 from emoji import UNICODE_EMOJI_ENGLISH
 import re
+import numpy as np
 
 
 class StatisticAugmenter(Augmenter):
@@ -37,15 +38,19 @@ def get_word_freq(words: list[str]) -> dict:
     return dict(Counter(words))
 
 
-def get_outliers(word_freq: dict) -> dict:
+def get_num_outliers(word_freq: dict) -> int:
     outliers = {}
-    ratio = 0.1
+    ratio = 0.2
+
+    if len(word_freq.keys()) < 10:
+        # If there is not enough words return 0
+        return 0
 
     for word, freq in word_freq.items():
         if freq >= (len(word_freq.keys()) * ratio):
             outliers[word] = freq
 
-    return outliers
+    return len(outliers.keys())
 
 
 def get_num_of_urls(string: str) -> int:
@@ -58,11 +63,12 @@ def get_non_alphanumeric(string: str) -> int:
     return len([char for char in string if not char.isalnum()])
 
 
-pattern_same_punctuation = re.compile("(([-/\\\\()!\"+,&'.])\\2+)")
+pattern_same_punctuation = re.compile("(([-/\\\\()!\"+,&'])\\2+)")
+pattern_any_punctuation = re.compile("([-/\\\\!?']{2,})")
 
 
 def get_num_aggressive_char(words_fused: str) -> int:
-    match = pattern_same_punctuation.findall(words_fused)
+    match = pattern_any_punctuation.findall(words_fused)
     return len(match) if match is not None else 0
 
 
@@ -80,11 +86,22 @@ def get_num_uppercase(words: list[str]) -> int:
     )
 
 
-def get_statistic(words: list[str]) -> List[Union[int, dict]]:
+def get_distribution_metrics(word_freq: dict) -> Tuple[int, int]:
+
+    distribution = [value for value in word_freq.values()]
+
+    mean = np.mean(distribution)
+    variance = np.var(distribution)
+
+    return mean, variance
+
+
+def get_statistic(words: list[str]) -> List[int]:
     words_fused = " ".join(words)
     num_words = get_num_words(words)
     word_freq = get_word_freq(words)
-    outliers = get_outliers(word_freq)
+    mean, variance = get_distribution_metrics(word_freq)
+    num_outliers = get_num_outliers(word_freq)
     num_urls = get_num_of_urls(words_fused)
     num_non_alphanumeric = get_non_alphanumeric(words_fused)
     num_uppercase = get_num_uppercase(words)
@@ -93,8 +110,9 @@ def get_statistic(words: list[str]) -> List[Union[int, dict]]:
 
     return [
         num_words,
-        word_freq,
-        outliers,
+        mean,
+        variance,
+        num_outliers,
         num_urls,
         num_non_alphanumeric,
         num_uppercase,
