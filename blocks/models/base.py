5af975fbb5af482fc9f5790ec6c7c944c7dd70c4
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-07-15 12:32:14 +0200

    fix: Took out functions from Model to not override.

--- blocks/models/base.py
+++ blocks/models/base.py
@@ -7,27 +7,3 @@ from blocks.base import Block
 class Model(Block):
 
     config: BaseConfig
-
-    def __init__(self, id: Optional[str] = None) -> None:
-        pass
-
-    def load(self, pipeline_id: str, execution_order: int) -> None:
-        pass
-
-    def load_remote(self) -> None:
-        pass
-
-    def fit(self, dataset: pd.DataFrame, labels: Optional[pd.Series]) -> None:
-        raise NotImplementedError()
-
-    def predict(self, dataset: pd.DataFrame) -> pd.DataFrame:
-        raise NotImplementedError()
-
-    def is_fitted(self) -> bool:
-        raise NotImplementedError()
-
-    def save(self, pipeline_id: str) -> None:
-        pass
-
-    def save_remote(self, pipeline_id: str) -> None:
-        pass
