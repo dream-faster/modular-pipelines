f933e1b4fac51425f294f2c7339a7078bfdc7efb
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-07-21 11:52:19 +0200

    feat: Imported pipelines.

--- run_multi.py
+++ run_multi.py
@@ -1,3 +1,10 @@
 from run import run
 
-# from library.examples.
+from library.examples.hate_speech import (
+    nlp_huggingface,
+    nlp_huggingface_autocorrect,
+    nlp_sklearn_autocorrect,
+)
+
+for pipeline in [nlp_huggingface, nlp_huggingface_autocorrect, nlp_sklearn_autocorrect]:
+    run(pipeline)
