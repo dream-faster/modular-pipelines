da754b4ee09ca3557c467f32a435e524b42f87b5
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-07-26 17:18:20 +0200

    feat: Added print to hf login.

--- utils/huggingface_hub.py
+++ utils/huggingface_hub.py
@@ -2,5 +2,9 @@ from huggingface_hub import HfApi
 
 
 def initialize_huggingface(token: str) -> None:
-    api = HfApi()
-    api.set_access_token(token)
+    try:
+        api = HfApi()
+        api.set_access_token(token)
+        print("API token set.")
+    except:
+        print("Token couldn't be set")
