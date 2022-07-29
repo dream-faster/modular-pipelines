8fd6aadc9c552e5f4b69d121f82aacd7c43e6ab6
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-07-29 12:01:23 +0200

    feature(Models): added RandomModel, AllZerosModel, AllOnesModel

---
+++ blocks/models/random.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from blocks.models.base import Model
+from type import PredsWithProbs, DataType
+from typing import Optional, List
+import random
+
+
+class AllZerosModel(Model):
+
+    inputTypes = [DataType.Series, DataType.List, DataType.NpArray]
+    outputType = DataType.PredictionsWithProbs
+
+    def fit(self, dataset: pd.Series, labels: Optional[pd.Series]) -> None:
+        pass
+
+    def predict(self, dataset: pd.Series) -> List[PredsWithProbs]:
+        predictions = [0 for _ in dataset]
+        probabilities = [[1.0, 0.0] for _ in dataset]
+        return list(zip(predictions, probabilities))
+
+    def is_fitted(self) -> bool:
+        return True
+
+
+class AllOnesModel(Model):
+
+    inputTypes = [DataType.Series, DataType.List, DataType.NpArray]
+    outputType = DataType.PredictionsWithProbs
+
+    def fit(self, dataset: pd.Series, labels: Optional[pd.Series]) -> None:
+        pass
+
+    def predict(self, dataset: pd.Series) -> List[PredsWithProbs]:
+        predictions = [1 for _ in dataset]
+        probabilities = [[0.0, 2.0] for _ in dataset]
+        return list(zip(predictions, probabilities))
+
+    def is_fitted(self) -> bool:
+        return True
+
+
+class RandomModel(Model):
+
+    inputTypes = [DataType.Series, DataType.List, DataType.NpArray]
+    outputType = DataType.PredictionsWithProbs
+
+    def fit(self, dataset: pd.Series, labels: Optional[pd.Series]) -> None:
+        pass
+
+    def predict(self, dataset: pd.Series) -> List[PredsWithProbs]:
+        predictions = [random.randint(0, 1) for _ in dataset]
+        probabilities = [
+            [0.0 if item == 1 else 1.0, 1.0 if item == 1 else 0.0]
+            for item in predictions
+        ]
+        return list(zip(predictions, probabilities))
+
+    def is_fitted(self) -> bool:
+        return True
