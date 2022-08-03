3bf98a85eda0b51d04d21bfcc1f1da06b9689219
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-08-03 18:21:02 +0200

    feat: Adaptor now throws back the correct element of the tuple in a List.

--- blocks/adaptors/classification_output.py
+++ blocks/adaptors/classification_output.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 from .base import Adaptor
-from type import DataType
-from typing import Optional
+from type import DataType, BaseConfig, PredsWithProbs
+from typing import Optional, List
 
 
 class ClassificationOutputAdaptor(Adaptor):
     inputTypes = DataType.PredictionsWithProbs
-    outputType = DataType.Series
+    outputType = DataType.List
 
-    def __init__(self, select: int, id: Optional[str] = None):
-        assert select < 2, "ClassificationOutputAdaptor only supports direction 0 or 1"
+    def __init__(self, select: int):
+        super().__init__()
+        assert select < 2, "ClassificationOutputAdaptor only supports 0 or 1 for select"
 
-        self.id = self.__class__.__name__ if id is None else id
         self.select = select
 
-    def predict(self, dataset: DataType.PredictionsWithProbs) -> DataType.Series:
-        return dataset[self.select]
+    def predict(self, dataset: List[PredsWithProbs]) -> DataType.List:
+        return [data[self.select] for data in dataset]
