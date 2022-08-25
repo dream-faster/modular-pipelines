b0061955c5614791f386d63d0c9ba84f4e2abcfc
Daniel Szemerey <daniel@szemerey.eu>
2022-08-25 16:36:57 +0200

    feat: Added test for obj_to_dict

---
+++ tests/test_unittests.py
@@ -0,0 +1,35 @@
+from utils.dict import obj_to_dict, flatten
+import pandas as pd
+import numpy as np
+
+
+def get_random_obj():
+    class RandomChild:
+        def __init__(self, i):
+            self.id = "child " + str(i)
+            self.integer = 5
+            self.string = "hello this is a string"
+
+    class ChildwithChildren:
+        def __init__(self):
+            self.id = "child_with_children"
+            self.random_children = [RandomChild(i) for i in range(5)]
+
+    class RandomParentClass:
+        def __init__(self):
+            self.id = "random_parent_class"
+            self.random_children = [RandomChild(i) for i in range(5)]
+            self.integer = 5
+            self.string = "hello this is a string"
+            self.child_with_children = ChildwithChildren()
+            self.df = pd.DataFrame([[1, 2, 3], ["a", "b", "c"]])
+            self.a_numpy = np.array([1, 2, 3])
+
+    return RandomParentClass()
+
+
+def test_obj_to_dict():
+    random_obj = get_random_obj()
+    for k, v in flatten(obj_to_dict(random_obj)).items():
+        assert isinstance(k, str), f"{k} is not a string"
+        assert isinstance(v, (str, int, float)), f"{v} is not a string, int or float"
--- utils/dict.py
+++ utils/dict.py
@@ -1,6 +1,8 @@
-from typing import Any, List
+from typing import Any, List, Optional
 from enum import Enum
 from copy import copy
+import pandas as pd
+import numpy as np
 
 
 def is_custom_obj(obj: Any):
@@ -8,6 +10,7 @@ def is_custom_obj(obj: Any):
         type(obj).__module__ is not object.__module__
         and isinstance(obj, Enum) is False
         and hasattr(obj, "__dict__")
+        and isinstance(obj, np.ndarray) is False
     ):
         return True
     else:
@@ -32,7 +35,18 @@ def obj_to_dict(obj: Any) -> dict:
         if isinstance(value, List):
             obj_dict[key] = list_to_dict(value)
 
+        elif isinstance(value, np.ndarray):
+            obj_dict[key] = copy(pd.DataFrame(value).to_dict())
+
+        elif isinstance(value, pd.DataFrame) or isinstance(value, pd.Series):
+            obj_dict[key] = copy(value.to_dict())
+
         elif is_custom_obj(value):
             obj_dict[key] = obj_to_dict(value)
 
     return obj_dict
+
+
+def flatten(input: dict) -> dict:
+
+    return pd.json_normalize(input, sep="_").to_dict(orient="records")[0]
