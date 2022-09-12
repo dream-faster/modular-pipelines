fa74d399afac41b8aff2bb1a867a2324a07c986e
Daniel Szemerey <daniel@szemerey.eu>
2022-09-12 13:05:38 +0200

    feat: Added configuration files for the packages.

--- src/run_dev.py
+++ src/run_dev.py
@@ -1,26 +1,31 @@
 from library.experiments.hate_speech_multi_objective import multi_objective_experiments
 from library.experiments.hate_speech_baselines import all_experiments
+from src.type import Experiment
 
 from type import StagingConfig, StagingNames
 
 
 from run import run
-
-experiments_list = all_experiments
-
-
-dev_config = StagingConfig(
-    name=StagingNames.dev,
-    save_remote=False,
-    log_remote=True,
-    limit_dataset_to=60,
-)
-
-for experiment in experiments_list:
-    experiment.project_name = "hate-speech-DEV"
+from typing import List
 
 
-run(
-    experiments_list,
-    staging_config=dev_config,
-)
+def run_dev(experiments: List[Experiment]) -> None:
+
+    dev_config = StagingConfig(
+        name=StagingNames.dev,
+        save_remote=False,
+        log_remote=True,
+        limit_dataset_to=60,
+    )
+
+    for experiment in experiments:
+        experiment.project_name = "hate-speech-DEV"
+
+    run(
+        experiments,
+        staging_config=dev_config,
+    )
+
+
+if __name__ == "__main__":
+    run_dev(all_experiments)
